0d1f2bd002a337cae88e471510e26068f8a54bc1
devoalda <linuxes_mojoworld@aleeas.com>
2024-02-03 14:49:13 +0800

    test(Recipe Test): Updated Tests for Recipe
    Updated:
    - Test cases for Recipe
    - Viewset for Updating of recipe (PUT request)
    
    Added:
    - Test image for tests

--- Backend/recipe/tests/test_recipe.py
+++ Backend/recipe/tests/test_recipe.py
@@ -1,42 +1,70 @@
+import base64
+import os
+
+from django.conf import settings
 from django.contrib.auth.models import User
 from django.test import TestCase
-from django.urls import reverse
+from django.urls import reverse_lazy
+from recipe.models import Recipe
 from rest_framework import status
 from rest_framework.test import APIClient
 
-from recipe.models import Recipe
-
 
 class RecipeViewSetTestCase(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.user = User.objects.create_user(username='testuser', password='testpass')
+        cls.image_path = os.path.join(settings.BASE_DIR, 'recipe/tests/test_image.jpg')
+        cls.recipe_name = 'Test_Recipe'
+        with open(cls.image_path, 'rb') as image_file:
+            image_data = base64.b64encode(image_file.read()).decode('utf-8')
+
+            # Construct image data in the format "data:image/jpeg;base64,<base64-encoded-data>;filename=<filename>"
+            image_data = f"data:image/jpeg;base64,{image_data};filename={cls.recipe_name}.jpg"
+
+        cls.recipe_data = {"title" : "Test Recipe", "instructions": "Test Instructions", "cooking_time": 30,
+                           "budget": "100", "image": image_data}
+
     def setUp(self):
-        self.user = User.objects.create_user(username='testuser', password='testpass')
         self.client = APIClient()
         self.client.force_authenticate(user=self.user)
-        self.recipe_data = {'title': 'Test Recipe', 'instructions': 'Test Instructions', 'cooking_time': 30,
-                            'image': 'http://testimage.com', 'budget': 100, }
-        self.response = self.client.post(reverse('recipe-list'), self.recipe_data, format='json')
+        self.response = self.client.post(reverse_lazy('recipe-list'), data=self.recipe_data, format='json')
+
+    def tearDown(self):
+        recipe = Recipe.objects.first()
+        if recipe and recipe.image:
+            print(f"Deleting image at {os.path.join(settings.MEDIA_ROOT, recipe.image.path)}")
+            os.remove(os.path.join(settings.MEDIA_ROOT, recipe.image.path))
 
     def test_create_recipe(self):
         self.assertEqual(self.response.status_code, status.HTTP_201_CREATED)
-        self.assertEqual(Recipe.objects.count(), 1)
-        self.assertEqual(Recipe.objects.get().title, 'Test Recipe')
+        self.assertQuerysetEqual(Recipe.objects.all(), ['Test Recipe'], transform=str)
 
     def test_get_recipe(self):
         recipe = Recipe.objects.get()
-        response = self.client.get(reverse('recipe-detail', kwargs={'pk': recipe.id}), format='json')
+        response = self.client.get(reverse_lazy('recipe-detail', kwargs={'pk': recipe.id}), format='json')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertContains(response, 'Test Recipe')
 
     def test_update_recipe(self):
         recipe = Recipe.objects.get()
-        new_data = {'title' : 'Updated Recipe', 'instructions': 'Updated Instructions', 'cooking_time': 45,
-                    'budget': 150, }
-        response = self.client.put(reverse('recipe-detail', kwargs={'pk': recipe.id}), new_data, format='json')
+        self.recipe_name = 'Updated_Recipe'
+        with open(self.image_path, 'rb') as image_file:
+            image_data = base64.b64encode(image_file.read()).decode('utf-8')
+
+            # Construct image data in the format "data:image/jpeg;base64,<base64-encoded-data>;filename=<filename>"
+            image_data = f"data:image/jpeg;base64,{image_data};filename={self.recipe_name}.jpg"
+
+        new_data = {"title" : "Updated Recipe", "instructions": "Updated Instructions", "cooking_time": 45,
+                    "budget": 150, "image": image_data}
+
+        response = self.client.put(reverse_lazy('recipe-detail', kwargs={'pk': recipe.id}), data=new_data,
+                                   format='json')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(Recipe.objects.get().title, 'Updated Recipe')
 
     def test_delete_recipe(self):
         recipe = Recipe.objects.get()
-        response = self.client.delete(reverse('recipe-detail', kwargs={'pk': recipe.id}), format='json')
+        response = self.client.delete(reverse_lazy('recipe-detail', kwargs={'pk': recipe.id}), format='json')
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
-        self.assertEqual(Recipe.objects.count(), 0)
+        self.assertQuerysetEqual(Recipe.objects.all(), [])
--- Backend/recipe/views.py
+++ Backend/recipe/views.py
@@ -5,54 +5,85 @@ from recipe.models import Recipe
 from recipe.serializers import RecipeSerializer
 from rest_framework import viewsets, status, permissions
 from rest_framework.parsers import JSONParser
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
+
+
+class IsOwnerOrAdmin(permissions.BasePermission):
+    """
+    Custom permission to only allow owners of an object or admin users to edit it.
+    """
+
+    def has_object_permission(self, request, view, obj):
+        return obj.author == request.user or request.user.is_staff
 
 
 class RecipeViewSet(viewsets.ModelViewSet):
     queryset = Recipe.objects.all()
     serializer_class = RecipeSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [IsAuthenticated]
     parser_classes = [JSONParser]
 
+    def get_permissions(self):
+        if self.action in ['update', 'partial_update']:
+            self.permission_classes = [IsOwnerOrAdmin]
+        return super(RecipeViewSet, self).get_permissions()
+
     def create(self, request, *args, **kwargs):
-        if request.content_type == 'application/json':
-            data = request.data.copy()
-            image_data = data.pop('image', None)
+        data = request.data.copy()
+        image_data = data.pop('image', None)
 
-            if image_data:
-                try:
-                    image_file = self.extract_image_from_data(image_data)
-                    data['image'] = image_file
-                except ValueError as e:
-                    return self.invalid_image_response(str(e))
+        if image_data:
+            image_file = self.extract_image_from_data(image_data)
+            data['image'] = image_file
 
-            serializer = self.get_and_validate_serializer(data)
-            serializer.save(author=self.request.user)
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        else:
-            return self.unsupported_media_type_response()
+        serializer = self.get_serializer(data=data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save(author=request.user)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
 
-    def extract_image_from_data(self, image_data):
+    def extract_image_from_data(self, image_data: str)-> ContentFile:
+        """
+        Extracts the image from the data and returns a ContentFile object
+        """
         delimiter = ';base64,'
         filename_delimiter = ';filename='
 
         parts = image_data.split(delimiter)
-        # metadata = parts[0]
         base64_data = parts[1]
 
         filename_parts = base64_data.split(filename_delimiter)
         image_name = filename_parts[1].strip('"')
 
-        return ContentFile(base64.b64decode(base64_data), name=image_name)
+        return ContentFile(base64.b64decode(filename_parts[0]), name=image_name)
 
-    def invalid_image_response(self, error_message):
-        return Response({'error': f'Invalid image data: {error_message}'}, status=status.HTTP_400_BAD_REQUEST)
+    def update(self, request, *args, **kwargs):
+        data = request.data.copy()
+        image_data = data.pop('image', None)
 
-    def get_and_validate_serializer(self, data):
-        serializer = self.get_serializer(data=data)
+        if image_data:
+            image_file = self.extract_image_from_data(image_data)
+            data['image'] = image_file
+
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=data)
         serializer.is_valid(raise_exception=True)
-        return serializer
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
-    def unsupported_media_type_response(self):
-        return Response({'error': 'Unsupported content type. Only JSON requests are accepted.'},
-                        status=status.HTTP_415_UNSUPPORTED_MEDIA_TYPE)
+    def partial_update(self, request: JSONParser, *args, **kwargs) -> Response:
+        """
+        Handle partial update of the recipe
+        """
+        data = request.data.copy()
+        image_data = data.pop('image', None)
+
+        if image_data:
+            image_file = self.extract_image_from_data(image_data)
+            data['image'] = image_file
+
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=data, partial=True)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_200_OK)
