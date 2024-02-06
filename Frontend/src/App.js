6d97fc4441659a23c9741f8c521b8665f48425fc
MayDay9939 <lowhongshengjovian@gmail.com>
2024-02-06 13:49:02 +0800

    restructure the codes and fix a smol issue

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
-import Header from "./components/Header";
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Layout from './components/Layout';
+import Registration from './pages/Registration';
+import Login from './pages/Login';
+import Home from './pages/Home';
+import Error from './pages/Error';
+import Logout from './pages/Logout';
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <Header />
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Registration />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/logout" element={<Logout />} />
+          <Route
+              path="/home"
+              element={
+                <Layout>
+                  <Home />
+                </Layout>
+              }
+          />
+          <Route path="*" element={<Error />} />
+        </Routes>
+      </Router>
   );
 }
 
