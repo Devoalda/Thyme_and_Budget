4066091bc90b686f5df3ee4ffc61d087e7d50d3d
devoalda <linuxes_mojoworld@aleeas.com>
2024-02-17 10:52:27 +0800

    fix(Location Serializer): Hotfix for location serilizer

--- Backend/thyme_and_budget_app/serializers/locationSerializer.py
+++ Backend/thyme_and_budget_app/serializers/locationSerializer.py
@@ -1,7 +1,12 @@
 from rest_framework import serializers
+
 from ..models import Location
 
+
 class LocationSerializer(serializers.ModelSerializer):
+    location = serializers.CharField(required=False)
+    address = serializers.CharField(required=False)
+
     class Meta:
         model = Location
         fields = ['id', 'location', 'address', 'postal_code']
