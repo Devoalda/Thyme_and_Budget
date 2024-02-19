8ec9c5e4e2d07b920fe89a58a715c5078db24a96
richie <wrongqir@icloud.com>
2024-02-19 15:42:19 +0800

    update onemap api call functions

--- location_service/update_api_key.py
+++ location_service/update_api_key.py
@@ -3,6 +3,7 @@ import requests
 from pathlib import Path
 import environ
 
+
 def update_api_key():
     BASE_DIR = Path(__file__).resolve().parent.parent
     environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
