db4afabb54e7496b8c992e98234a427bce5dd93d
devoalda <linuxes_mojoworld@aleeas.com>
2024-02-20 13:25:52 +0800

    refactor(Location): Location Update
    Updated location with RBAC
    
    Modified Migrations and test cases

--- thyme_and_budget_app/tests/test_FoodItem.py
+++ thyme_and_budget_app/tests/test_FoodItem.py
@@ -36,11 +36,12 @@ class FoodTests(APITestCase):
         expiry_date_str = expiry_date.strftime('%Y-%m-%d')
 
         # Set up the food data
-        self.food_data = {'name': '1234', 'expiry_date': expiry_date_str, 'quantity': 100000, 'image': self.data, 'location': self.location.id}
+        self.food_data = {'name': '1234', 'expiry_date': expiry_date_str, 'quantity': 100000, 'image': self.data,
+                          'location': self.location.id, 'donor': self.user.id}
 
         # Create a FoodItem object
         self.food_item = FoodItem.objects.create(name='1234', expiry_date=expiry_date_str, quantity=100000,
-                image=self.data, location=self.location, donor=self.user)
+                                                 image=self.data, location=self.location, donor=self.user)
 
     def test_authenticated_user_can_create_food(self):
         # Modify image to be a base64 string
@@ -80,7 +81,7 @@ class FoodTests(APITestCase):
     def test_unauthenticated_user_cannot_update_food(self):
         self.client.logout()
         response = self.client.patch(reverse('food-detail', kwargs={'pk': self.food_item.pk}), self.food_data,
-                                   format='json')
+                                     format='json')
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
 
     def test_unauthenticated_user_can_read_food(self):
@@ -95,3 +96,11 @@ class FoodTests(APITestCase):
     def test_cannot_delete_non_existent_food(self):
         response = self.client.delete(reverse('food-detail', kwargs={'pk': 999}))
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_user_can_add_food_with_another_location(self):
+        location2 = Location.objects.create(postal_code='138683')
+        self.food_data['image'] = self.image.decode('utf-8')
+        self.food_data['location'] = location2.id
+        response = self.client.post(reverse('food-list'), self.food_data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(response.data['location'], location2.id)
