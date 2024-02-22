1c018ac5f0cc5187b7ed4c24743574c1d0ee588a
devoalda <linuxes_mojoworld@aleeas.com>
2024-02-22 18:46:09 +0800

    refactor(Settings): Moved settings to python package

--- account/views.py
+++ account/views.py
@@ -9,24 +9,6 @@ from rest_framework.views import APIView
 from .serializers import (UserRegistrationSerializer, UserLoginSerializer, UserListSerializer)
 
 
-class AuthUserRegistrationView(APIView):
-    serializer_class = UserRegistrationSerializer
-    permission_classes = (AllowAny,)
-
-    def post(self, request):
-        serializer = self.serializer_class(data=request.data)
-        valid = serializer.is_valid()
-
-        if valid:
-            serializer.save()
-            status_code = status.HTTP_201_CREATED
-
-            response = {'success': True, 'statusCode': status_code, 'message': 'User successfully registered!',
-                        'user'   : serializer.validated_data}
-
-            return Response(response, status=status_code)
-
-
 class AuthUserLoginView(APIView):
     serializer_class = UserLoginSerializer
     permission_classes = (AllowAny,)
@@ -38,10 +20,10 @@ class AuthUserLoginView(APIView):
         if valid:
             status_code = status.HTTP_200_OK
 
-            response = {'success'          : True, 'statusCode': status_code, 'message': 'User logged in successfully',
-                        'access'           : serializer.data['access'], 'refresh': serializer.data['refresh'],
+            response = {'success': True, 'statusCode': status_code, 'message': 'User logged in successfully',
+                        'access': serializer.data['access'], 'refresh': serializer.data['refresh'],
                         'authenticatedUser': {'username': serializer.validated_data['username'],
-                                              'role'    : serializer.validated_data['role']}}
+                                              'role': serializer.validated_data['role']}}
 
             return Response(response, status=status_code)
 
@@ -59,7 +41,7 @@ class AuthUserRegistrationView(APIView):
             status_code = status.HTTP_201_CREATED
 
             response = {'success': True, 'statusCode': status_code, 'message': 'User successfully registered!',
-                        'user'   : serializer.validated_data}
+                        'user': serializer.validated_data}
 
             return Response(response, status=status_code)
 
@@ -75,10 +57,10 @@ class AuthUserLoginView(APIView):
         if valid:
             status_code = status.HTTP_200_OK
 
-            response = {'success'          : True, 'statusCode': status_code, 'message': 'User logged in successfully',
-                        'access'           : serializer.data['access'], 'refresh': serializer.data['refresh'],
+            response = {'success': True, 'statusCode': status_code, 'message': 'User logged in successfully',
+                        'access': serializer.data['access'], 'refresh': serializer.data['refresh'],
                         'authenticatedUser': {'username': serializer.validated_data['username'],
-                                              'role'    : serializer.validated_data['role']}}
+                                              'role': serializer.validated_data['role']}}
 
             return Response(response, status=status_code)
 
@@ -107,7 +89,6 @@ class UserRetrieveUpdateDestroyView(APIView):
         instance = self.get_object()
         instance.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
-
 
 
 class CheckUserStatusView(APIView):
---
+++ thyme_and_budget/settings/base.py
@@ -0,0 +1,58 @@
+# settings/base.py
+from .env_singleton import EnvSingleton
+
+env = EnvSingleton().env
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = env('DEBUG', default=True)
+
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default=['*'])
+
+# Application definition
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'rest_framework',
+    'rest_framework_simplejwt',
+    'account',
+    'corsheaders',
+    'thyme_and_budget_app',
+]
+
+AUTH_USER_MODEL = 'account.User'
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+ROOT_URLCONF = 'thyme_and_budget.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND' : 'django.template.backends.django.DjangoTemplates',
+        'DIRS'    : [],
+        'APP_DIRS': True,
+        'OPTIONS' : {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = 'thyme_and_budget.wsgi.application'
